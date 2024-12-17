0de6a2052e20f0b5bfc2640cd776757c2981e796
Michele Baldessari <michele@acksyn.org>
2024-12-17 15:41:55 +0100

    utils1

---
+++ utils/erb-templating.rb
@@ -0,0 +1,27 @@
+require 'erb'
+require 'yaml'
+
+begin
+  metadata = YAML.load_file(ARGV[0])
+rescue Errno::ENOENT
+  puts "Error: #{ARGV[0]} not found"
+  exit(1)
+rescue Psych::SyntaxError => e
+  puts "YAML syntax error: #{e.message}"
+  exit(1)
+end
+
+begin
+  template = File.read(ARGV[1])
+rescue Errno::ENOENT
+  puts "Error: #{ARGV[1]} not found"
+  exit(1)
+rescue => e
+  puts "Error: #{e.message}"
+  exit(1)
+end
+
+requirements = metadata['requirements']
+renderer = ERB.new(template)
+output = renderer.result(binding)
+puts output
